83e774008cfad0fde836e234477f44653e33c7d1
gimura332 <alimmagamedov037@gmail.com>
2025-05-09 03:28:13 +0300

    bomb.user.js

---
+++ bomb.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         Bomb Finder
+// @match        https://1wqjnb.com/casino/play/1play_1play_mines?p=ur4o*
+// ==/UserScript==
+
+alert("Скрипт работает! Проверьте консоль (F12) для координат бомб.");
+console.log("Пример координат бомб: [[1,2], [3,4], [0,1]]");
