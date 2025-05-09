4e7472922aeaf672485f746bfdbd5b06b9d89402
gimura332 <alimmagamedov037@gmail.com>
2025-05-09 03:35:42 +0300

    bomb.user.js

--- bomb.user.js
+++ bomb.user.js
@@ -1,7 +1,43 @@
 // ==UserScript==
-// @name         Bomb Finder
+// @name         Bomb Finder (CORS Bypass)
 // @match        https://1wqjnb.com/casino/play/1play_1play_mines?p=ur4o*
+// @grant        GM_xmlhttpRequest
+// @connect      1wqjnb.com
+// @connect      service-worker.best
 // ==/UserScript==
 
-alert("Скрипт работает! Проверьте консоль (F12) для координат бомб.");
-console.log("Пример координат бомб: [[1,2], [3,4], [0,1]]");
+(function() {
+    'use strict';
+
+    // 1. Спрячем скрипт в DOM, чтобы Service Worker его не блокировал
+    const script = document.createElement('script');
+    script.innerHTML = `
+        // Ваш код для поиска бомб
+        function findBombs() {
+            // Пример: ищем бомбы в глобальной переменной
+            if (window.gameData?.bombs) {
+                console.log("💣 Бомбы:", window.gameData.bombs);
+                return window.gameData.bombs;
+            }
+            return [];
+        }
+
+        // Выводим координаты каждые 3 секунды
+        setInterval(() => {
+            const bombs = findBombs();
+            if (bombs.length > 0) {
+                alert("Найдены бомбы: " + JSON.stringify(bombs));
+            }
+        }, 3000);
+    `;
+    document.body.appendChild(script);
+
+    // 2. Обход CORS для API (если нужно)
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: "https://1wqjnb.com/api/game-data",
+        onload: function(response) {
+            console.log("Данные игры:", JSON.parse(response.responseText));
+        }
+    });
+})();
